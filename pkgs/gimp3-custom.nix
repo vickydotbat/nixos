e8bf241045b3229069169c89361736c634c637bb
vickydotbat <vickydotbat@tutamail.com>
2026-05-31 10:57:00 +0200

    Gimp3 custom fix

--- pkgs/gimp3-custom.nix
+++ pkgs/gimp3-custom.nix
@@ -1,13 +1,19 @@
 {
   bash,
   coreutils,
+  gdk-pixbuf,
   gimp,
   gimp3-with-plugins,
+  gexiv2,
+  glib,
+  gobject-introspection,
+  harfbuzz,
   gtk3,
   gegl,
   babl,
   lib,
   makeWrapper,
+  pango,
   python3,
   stdenvNoCC,
   symlinkJoin,
@@ -20,6 +26,25 @@ let
     ]
   );
 
+  pluginTypelibPath = lib.makeSearchPath "lib/girepository-1.0" [
+    (lib.getLib babl)
+    (lib.getLib gdk-pixbuf)
+    (lib.getLib gegl)
+    (lib.getLib gexiv2)
+    (lib.getLib gimp)
+    (lib.getLib glib)
+    (lib.getLib gobject-introspection)
+    (lib.getLib harfbuzz)
+    (lib.getLib gtk3)
+    (lib.getLib pango)
+  ];
+
+  pluginRuntimePath = lib.makeBinPath [
+    gimpPluginPython
+    coreutils
+    bash
+  ];
+
   gimpPlugins = stdenvNoCC.mkDerivation {
     pname = "gimp-local-plugins";
     version = "0";
@@ -28,6 +53,7 @@ let
 
     nativeBuildInputs = [
       gimpPluginPython
+      makeWrapper
     ];
 
     installPhase = ''
@@ -46,6 +72,12 @@ let
       find $out/lib/gimp/3.0/plug-ins -type f \( -name '*.py' -o -name '*.sh' \) -exec chmod +x {} +
       patchShebangs $out/lib/gimp/3.0/plug-ins
 
+      while IFS= read -r plugin; do
+        wrapProgram "$plugin" \
+          --set PATH ${pluginRuntimePath} \
+          --prefix GI_TYPELIB_PATH : ${pluginTypelibPath}
+      done < <(find $out/lib/gimp/3.0/plug-ins -type f \( -name '*.py' -o -name '*.sh' \))
+
       runHook postInstall
     '';
   };
@@ -60,22 +92,29 @@ symlinkJoin {
   nativeBuildInputs = [ makeWrapper ];
 
   postBuild = ''
-    wrapProgram $out/bin/gimp \
-      --set PATH ${
-        lib.makeBinPath [
-          gimpPluginPython
-          coreutils
-          bash
-        ]
-      } \
-      --prefix GI_TYPELIB_PATH : ${
-        lib.makeSearchPath "lib/girepository-1.0" [
-          gimp
-          gtk3
-          gegl
-          babl
-        ]
-      }
+    rm -f \
+      $out/bin/gimp \
+      $out/bin/gimp-3 \
+      $out/bin/gimp-3.0 \
+      $out/bin/gimp-console \
+      $out/bin/gimp-console-3 \
+      $out/bin/gimp-console-3.0
+
+    makeWrapper ${gimp}/bin/gimp-3.0 $out/bin/gimp-3.0 \
+      --set GIMP3_PLUGINDIR "$out/lib/gimp/3.0" \
+      --set GIMP3_DATADIR "$out/share/gimp/3.0" \
+      --set PATH ${pluginRuntimePath} \
+      --prefix GI_TYPELIB_PATH : ${pluginTypelibPath}
+    ln -s gimp-3.0 $out/bin/gimp
+    ln -s gimp-3.0 $out/bin/gimp-3
+
+    makeWrapper ${gimp}/bin/gimp-console-3.0 $out/bin/gimp-console-3.0 \
+      --set GIMP3_PLUGINDIR "$out/lib/gimp/3.0" \
+      --set GIMP3_DATADIR "$out/share/gimp/3.0" \
+      --set PATH ${pluginRuntimePath} \
+      --prefix GI_TYPELIB_PATH : ${pluginTypelibPath}
+    ln -s gimp-console-3.0 $out/bin/gimp-console
+    ln -s gimp-console-3.0 $out/bin/gimp-console-3
   '';
 
   meta = {
