ec5340cb9610ae554afe8694b2726fbf0f85faa5
vickydotbat <vickydotbat@tutamail.com>
2026-06-03 12:41:47 +0200

    Declarative run0 defaults

--- modules/nixos/security/run0.nix
+++ modules/nixos/security/run0.nix
@@ -3,12 +3,35 @@
   lib,
   ...
 }:
-# TODO: Run0 should in theory replace sudo on hardened systems. It is also a much better default, but analyze first. Let there be a choice between sudo and run0.
 let
   cfg = config.theorem.nixos.security.run0-sudo;
+  quotedUsers = lib.concatMapStringsSep ", " (user: ''"${user}"'') cfg.authenticationCacheUsers;
+  quotedGroups = lib.concatMapStringsSep ", " (group: ''"${group}"'') cfg.authenticationCacheGroups;
 in
 {
-  options.theorem.nixos.security.run0-sudo.enable = lib.mkEnableOption "Run0 instead of sudo";
+  options.theorem.nixos.security.run0-sudo = {
+    enable = lib.mkEnableOption "Run0 instead of sudo";
+
+    authenticationCacheUsers = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = ''
+        User names allowed to keep Polkit authentication for one NixOS
+        administrative operation. Keep this narrow; every named account becomes
+        part of the host's elevation path.
+      '';
+    };
+
+    authenticationCacheGroups = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ "wheel" ];
+      description = ''
+        Group names allowed to keep Polkit authentication for one NixOS
+        administrative operation. `wheel` mirrors the usual administrator
+        boundary while keeping the rule declarative.
+      '';
+    };
+  };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [
@@ -41,21 +64,21 @@ in
       };
 
       # Reduce friction of password entries.
-      # TODO: Populate this on a per-user basis.
-      polkit.extraConfig =
-        let
-          username = "fill this in"; # TODO: Per user.
-        in
-        ''
-          polkit.addRule(function(action, subject) {
-            if (subject.user == "${username}") {
-              if (action.id.indexOf("org.nixos") == 0) {
-                polkit.log("Caching admin authentication for single NixOS operation");
-                return polkit.Result.AUTH_ADMIN_KEEP;
-              }
-            }
+      polkit.extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          var users = [${quotedUsers}];
+          var groups = [${quotedGroups}];
+          var permittedUser = users.indexOf(subject.user) >= 0;
+          var permittedGroup = groups.some(function(group) {
+            return subject.isInGroup(group);
           });
-        '';
+
+          if ((permittedUser || permittedGroup) && action.id.indexOf("org.nixos") == 0) {
+            polkit.log("Caching admin authentication for single NixOS operation");
+            return polkit.Result.AUTH_ADMIN_KEEP;
+          }
+        });
+      '';
 
     };
   };
