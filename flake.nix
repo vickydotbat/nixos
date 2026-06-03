f2c9ded2a93bcaf7e7638d3dd6f9ff2127badd90
vickydotbat <vickydotbat@tutamail.com>
2026-06-03 18:07:04 +0200

    Clean up nix-index-database format

--- flake.nix
+++ flake.nix
@@ -60,8 +60,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
