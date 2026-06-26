0e7cb6432a3452ab211cb4406ce88fa061ae4a46
vickydotbat <vickydotbat@tutamail.com>
2026-06-26 12:52:47 +0200

    sillytavern with persistence

--- users/vicky/profiles/packages.nix
+++ users/vicky/profiles/packages.nix
@@ -1,1 +1,15 @@
-{ pkgs, ... }: { home.packages = with pkgs; [ sillytavern ]; }
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  home.packages = with pkgs; [ sillytavern ];
+
+  home.persistence."/nix/persist" = lib.mkIf config.theorem.home.base.persistence.enable {
+    directories = [
+      ".local/share/SillyTavern"
+    ];
+  };
+}
